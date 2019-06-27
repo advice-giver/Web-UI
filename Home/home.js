5ee37b7356eb07f5bb711317fe90fa09c95a8a40
Reese Kunz <reesekunz21@gmail.com>
2019-06-27 14:22:41 -0700

    cleaned up home page javascript

--- Home/home.js
+++ Home/home.js
@@ -1,61 +1,35 @@
-// Changing buttons when moused over
+// Change button colors on mouseover
 
-let button = document.querySelector("button");
-
-function changeButton() {
+let buttons = document.querySelectorAll("button");
+function changeButton(button) {
   button.style.color = "yellow";
 }
 
-button.addEventListener("mouseover", changeButton);
+buttons.forEach(button => {
+  button.addEventListener("mouseover", () => {
+    changeButton(button);
+  });
+});
+// Change headers color on mouseover
 
-let button2 = document.querySelector("#footerbutton");
-
-function changeButton2() {
-  button2.style.color = "yellow";
-}
-
-button2.addEventListener("mouseover", changeButton2);
-
-// Changing headers color when moused over 
-
-let header = document.querySelector("#worksheader");
-
-function changeHeader() {
+let headers = document.querySelectorAll(".sectionheader");
+function changeHeader(header) {
   header.style.color = "darkred";
 }
 
-header.addEventListener("mouseover", changeHeader);
+headers.forEach(header => {
+  header.addEventListener("mouseover", () => {
+    changeHeader(header);
+  });
+});
+// Highlight text on mouseover
 
-
-let header2 = document.querySelector(".agheader");
-
-function changeHeader2() {
-  header2.style.color = "darkred";
-}
-
-header2.addEventListener("mouseover", changeHeader2);
-
-let header3 = document.querySelector(".helpedheader");
-
-function changeHeader3() {
-  header3.style.color = "darkred";
-}
-
-header3.addEventListener("mouseover", changeHeader3);
-
-
-// Increase card size text on mouseover 
-
-let highlights = document.querySelectorAll("strong")
+let highlights = document.querySelectorAll("strong");
 function changeColor(highlight) {
- highlight.style.backgroundColor = "yellow";
+  highlight.style.backgroundColor = "yellow";
 }
-
-
 highlights.forEach(highlight => {
- highlight.addEventListener('mouseenter', () => {
-    changeColor(highlight)
-  })
-})
-
-
+  highlight.addEventListener("mouseover", () => {
+    changeColor(highlight);
+  });
+});
