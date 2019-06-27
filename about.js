6e9c282afbdfa642d55d7505268e7baa77cf16a5
Reese Kunz <reesekunz21@gmail.com>
2019-06-27 13:44:02 -0700

    added event listener to change font and border on team names when clicked

--- about.js
+++ about.js
@@ -37,3 +37,76 @@ function changeColor3() {
 
 meetTeam.addEventListener("mouseover", changeColor3);
 
+// Change team names to cursive w/ bottom border on click
+
+let cursive = document.querySelector("h4");
+
+function changeFont() {
+  cursive.style.borderBottom = "thin solid black";
+ cursive.style.fontFamily = "cursive";
+
+}
+
+cursive.addEventListener("click", changeFont);
+
+
+
+let cursive2 = document.querySelector("#two");
+
+function changeFont2() {
+  cursive2.style.fontFamily = "cursive";
+  cursive2.style.borderBottom = "thin solid black";
+
+}
+
+cursive2.addEventListener("click", changeFont2);
+
+let cursive3 = document.querySelector("#three");
+
+function changeFont3() {
+  cursive3.style.fontFamily = "cursive";
+  cursive3.style.borderBottom = "thin solid black";
+
+}
+
+cursive3.addEventListener("click", changeFont3);
+
+let cursive4 = document.querySelector("#four");
+
+function changeFont4() {
+  cursive4.style.fontFamily = "cursive";
+  cursive4.style.borderBottom = "thin solid black";
+
+}
+
+cursive4.addEventListener("click", changeFont4);
+
+let cursive5 = document.querySelector("#five");
+
+function changeFont5() {
+  cursive5.style.fontFamily = "cursive";
+  cursive5.style.borderBottom = "thin solid black";
+
+}
+
+cursive5.addEventListener("click", changeFont5);
+
+let cursive6 = document.querySelector("#six");
+
+function changeFont6() {
+  cursive6.style.fontFamily = "cursive";
+  cursive6.style.borderBottom = "thin solid black";
+
+}
+
+cursive6.addEventListener("click", changeFont6);
+
+let cursive7 = document.querySelector("#seven");
+
+function changeFont7() {
+  cursive7.style.fontFamily = "cursive";
+  cursive7.style.borderBottom = "thin solid black";
+
+}
+
+cursive7.addEventListener("click", changeFont7);
