700a0c6eb21a725e5e49a05339afbd8d31997c1b
Reese Kunz <reesekunz21@gmail.com>
2019-06-27 02:10:20 -0700

    changed header color on mouseover for about page

---
+++ about.js
@@ -0,0 +1,22 @@
+
+// Change color of header when mouseover 
+
+let mission = document.querySelector("h3");
+
+function changeColor ()
+{mission.style.color ="darkred";}
+
+
+mission.addEventListener("mouseover",changeColor);
+
+
+let mission2 = document.querySelector("#header2");
+
+function changeColor2 ()
+{mission2.style.color ="darkred";}
+
+
+mission2.addEventListener("mouseover",changeColor2);
+
+// Increase 
+
