3b8c2657639f11a446ab7eb4a42d0fb2ac632022
Reese Kunz <reesekunz21@gmail.com>
2019-06-27 01:53:53 -0700

    changed mouseover button color

---
+++ Home/home.js
@@ -0,0 +1,21 @@
+
+// Changing buttons when moused over 
+
+let button = document.querySelector("button");
+
+function changeButton ()
+{button.style.color ="yellow";}
+
+
+button.addEventListener("mouseover",changeButton);
+
+
+let button2 = document.querySelector("#footerbutton");
+
+function changeButton2 ()
+{button2.style.color ="yellow";}
+
+
+button2.addEventListener("mouseover",changeButton2);
+
+
