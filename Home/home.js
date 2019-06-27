86a351aa13f7a26f270a0cb58b2484402efc3f8e
Reese Kunz <reesekunz21@gmail.com>
2019-06-27 11:35:49 -0700

    added change color mouseover to home page headers

--- Home/home.js
+++ Home/home.js
@@ -15,3 +15,33 @@ function changeButton2() {
 }
 
 button2.addEventListener("mouseover", changeButton2);
+
+// Changing headers color when moused over 
+
+let header = document.querySelector("#worksheader");
+
+function changeHeader() {
+  header.style.color = "darkred";
+}
+
+header.addEventListener("mouseover", changeHeader);
+
+
+let header2 = document.querySelector(".agheader");
+
+function changeHeader2() {
+  header2.style.color = "darkred";
+}
+
+header2.addEventListener("mouseover", changeHeader2);
+
+let header3 = document.querySelector(".helpedheader");
+
+function changeHeader3() {
+  header3.style.color = "darkred";
+}
+
+header3.addEventListener("mouseover", changeHeader3);
+
+
+// 
